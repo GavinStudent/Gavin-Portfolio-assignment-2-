147cae8d5673c21960cdcb915545299d6095f9af
GavinStudent <gavinpaul.dizon@ontariotechu.net>
2025-11-05 14:55:52 -0500

    Initial commit

---
+++ WebProject/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+router.get('/contactus', function(req, res, next) {
+  res.render('index', { title: 'Contact Us' });
+});
+
+module.exports = router;
